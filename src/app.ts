dd572475c272f30b49ed61af3da29c90ee035680
43MorenoLuis <lmoreno9943@gmail.com>
2022-11-21 19:56:26 -0500

    Config Connect MongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import "dotenv/config"
 import express from 'express'
 import cors from 'cors'
+
+import { dbConnect } from './config/mongo'
 import { router } from './routes'
 
 const PORT = process.env.PORT || 3005
 const app = express()
 
 app.use(cors())
+
+dbConnect()
+
 app.use(router)
 
 app.listen(PORT, () => {
